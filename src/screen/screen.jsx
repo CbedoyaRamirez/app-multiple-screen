8211263092b53518418c338c20e03fb9a42bd682
carlos <cbedoya@agrosavia.co>
2023-11-24 11:59:13 -0500

    Se ajusta el componente screen

--- src/screen/screen.jsx
+++ src/screen/screen.jsx
@@ -10,27 +10,27 @@ function Screen() {
         <main>
             <section className="caja" >
                 <h3>Pantalla 1</h3>
-                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/Q6GkCmXiGhE?si=AbfLo1jF27GegxEM&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
+                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/IWMi1UE113s?si=oyfrvRlMXjheKaHe&amp;controls=1&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
             </section>
             <section className="caja">
                 <h3>Pantalla 2</h3>
-                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/pv1ZQRnOTIs?si=qOBSLlPzmBNaTqWt&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
+                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/mzdfGCdNSHQ?si=m1UqSThjzLmnFXgY&amp;controls=1&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
             </section>
             <section className="caja">
                 <h3>Pantalla 3</h3>
-                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/BNVEBwbA3c8?si=-g646SeNTND7X-7s&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
+                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/Y-IlMeCCtIg?si=J2QjcbPKYFtJDfTG&amp;controls=1&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
             </section>
             <section className="caja">
                 <h3>Pantalla 4</h3>
-                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/tsStUN73_6I?si=JnhWaQzUJ9pGLG-g&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
+                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/tsStUN73_6I?si=JnhWaQzUJ9pGLG-g&amp;controls=1&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
             </section>
             <section className="caja">
                 <h3>Pantalla 5</h3>
-                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/4e8Iw3Frf1A?si=9ytwtV8rqXNDfZRM&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
+                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/4e8Iw3Frf1A?si=9ytwtV8rqXNDfZRM&amp;controls=1&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
             </section>
             <section className="caja">
                 <h3>Pantalla 6</h3>
-                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/JSUQFTeymJ0?si=SCpLx4ZnoCIkOKUL&amp;controls=0&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
+                <iframe width="100%" height="93%" src="https://www.youtube.com/embed/8xykEUmK22s?si=z0Wmc65rngJ-1_jN&amp;controls=1&amp;autoplay=1&amp;mute=1&amp;enablejsapi=1" title="YouTube video player" frameBorder="0" allowFullScreen></iframe>
             </section>
         </main >
     )
